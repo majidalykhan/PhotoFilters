7dac7d1cf74b281223209293a6317d4f6b0801a4
majidalykhan <majidalykhan@gmail.com>
2019-11-29 22:25:56 +0500

    Firebase

--- app/src/main/java/com/example/photofilters/Login.java
+++ app/src/main/java/com/example/photofilters/Login.java
@@ -1,17 +1,37 @@
 package com.example.photofilters;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
-    Button reg;
+    ImageButton login;
     TextView registerhere;
+
+    EditText email;
+    EditText password;
+
+    ProgressBar pb;
+
+    FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +47,43 @@ public class Login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        email = findViewById(R.id.emaillogin);
+        password = findViewById(R.id.passwordlogin);
+        login =  findViewById(R.id.loginbtn);
+        pb = findViewById(R.id.progressBar2);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userLogin();
+            }
+        });
+
     }
+
+    private void userLogin(){
+
+        pb.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                pb.setVisibility(View.GONE);
+                if(task.isSuccessful()){
+                    Intent intent = new Intent (Login.this, Test.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/photofilters/Reg.java
+++ app/src/main/java/com/example/photofilters/Reg.java
@@ -1,17 +1,35 @@
 package com.example.photofilters;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Reg extends AppCompatActivity {
 
-    Button login;
+    ImageButton register;
     TextView loginhere;
+    EditText name;
+    EditText username;
+    EditText email;
+    EditText password;
+
+    ProgressBar pb;
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +42,38 @@ public class Reg extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), Login.class);
                 startActivity(intent);
+            }
+        });
+
+        name = findViewById(R.id.name);
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        register =  findViewById(R.id.regbtn);
+        pb = findViewById(R.id.progressBar);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pb.setVisibility(View.VISIBLE);
+                firebaseAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        pb.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(Reg.this, "Registeration Successful, Go to Login",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                            Toast.makeText(Reg.this, task.getException().getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
             }
         });
     }
