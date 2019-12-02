977fd0a932659d57787705c10c414b95bed4c37b
majidalykhan <majidalykhan@gmail.com>
2019-12-02 21:10:02 +0500

    Camera API
    Display camera and Take pictures

---
+++ app/src/main/java/com/example/photofilters/CameraActivity.java
@@ -0,0 +1,28 @@
+package com.example.photofilters;
+
+import android.hardware.Camera;
+import android.view.SurfaceView;
+
+public class CameraActivity extends Home {
+
+    private Camera mCamera;
+    private SurfaceView preview;
+
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // if you are using MediaRecorder, release it first
+        releaseCamera();              // release the camera immediately on pause event
+    }
+
+
+
+    private void releaseCamera(){
+        if (mCamera != null){
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+        }
+    }
+}
--- app/src/main/java/com/example/photofilters/Home.java
+++ app/src/main/java/com/example/photofilters/Home.java
@@ -2,10 +2,16 @@ package com.example.photofilters;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
@@ -15,10 +21,23 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import es.dmoral.toasty.Toasty;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
 
 public class Home extends AppCompatActivity {
 
+    public static final String TAG = "PhotoFilters";
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
 
     TextView user;
     TextView capturetext;
@@ -26,9 +45,10 @@ public class Home extends AppCompatActivity {
     ImageButton capture;
     ImageButton gallery;
 
-    Camera camera;
-    FrameLayout framerLayout;
-    ShowCamera showCamera;
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
+    FrameLayout preview;
 
 
     @Override
@@ -41,8 +61,6 @@ public class Home extends AppCompatActivity {
         capturetext = findViewById(R.id.capturetext);
         capture = findViewById(R.id.capture);
         gallery = findViewById(R.id.gallerybtn);
-
-        framerLayout = findViewById(R.id.framelayout);
 
         //Firebase instance
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
@@ -70,10 +88,120 @@ public class Home extends AppCompatActivity {
             }
         });
 
-        //Open Camera
-        camera = Camera.open();
-        showCamera = new ShowCamera(this,camera);
-        framerLayout.addView(showCamera);
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                mCamera.takePicture(null, null, mPicture);
+            }
+        });
+
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.framelayout);
+        preview.addView(mPreview);
+
     }
+
+    /** Create a file Uri for saving an image or video */
+    private Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+
+    private File getOutputMediaFile(int type) {
+
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+        return mediaFile;
+    }
+
+
+
+    //Check if device has camera
+    private boolean checkCamera(Context context) {
+            if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+                // this device has a camera
+                return true;
+            } else {
+                // no camera on this device
+                return false;
+            }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                Log.d(TAG, "Error creating media file, check storage permissions");
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
+
+
+
+
+
+
+
+
 
 }
