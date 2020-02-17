4ab5977d6a7ae69ca551fe2a83232ec194c6c716
majidalykhan <majidalykhan@gmail.com>
2020-02-18 01:09:14 +0500

    Arcore classes setup

--- app/src/main/java/com/example/photofilters/Home.java
+++ app/src/main/java/com/example/photofilters/Home.java
@@ -1,24 +1,44 @@
 package com.example.photofilters;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.res.Configuration;
 import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Surface;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
+
+import com.google.ar.core.AugmentedFace;
+import com.google.ar.core.TrackingState;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.rendering.Texture;
+import com.google.ar.sceneform.ux.AugmentedFaceNode;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -29,14 +49,10 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import es.dmoral.toasty.Toasty;
-
-import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
-import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
-
 public class Home extends AppCompatActivity {
 
-    public static final String TAG = "PhotoFilters";
+    private static final String TAG = Home.class.getSimpleName();
+    //public static final String TAG = "PhotoFilters";
     public static final int MEDIA_TYPE_IMAGE = 1;
     public static final int MEDIA_TYPE_VIDEO = 2;
     int currentCameraId = 0;
@@ -53,6 +69,16 @@ public class Home extends AppCompatActivity {
     private CameraPreview mPreview;
 
     FrameLayout preview;
+
+    private static final double MIN_OPENGL_VERSION = 3.0;
+
+
+    private FaceArFragment arFragment;
+    private Texture faceMeshTexture;
+
+    private ModelRenderable faceRegionsRenderable;
+
+    private final HashMap<AugmentedFace, AugmentedFaceNode> faceNodeMap = new HashMap<>();
 
 
     @Override
@@ -89,7 +115,6 @@ public class Home extends AppCompatActivity {
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
-
             }
         });
 
@@ -129,6 +154,79 @@ public class Home extends AppCompatActivity {
         FrameLayout preview = (FrameLayout) findViewById(R.id.framelayout);
         preview.addView(mPreview);
 
+        Arcore();
+
+    }
+
+    private void Arcore(){
+        //ArCore Augmented Faces
+
+        if (!checkIsSupportedDeviceOrFinish(this)) {
+            return;
+        }
+
+        setContentView(R.layout.activity_home);
+        arFragment = (FaceArFragment) getSupportFragmentManager().findFragmentById(R.id.face_fragment);
+
+        // Load the face regions renderable.
+        // This is a skinned model that renders 3D objects mapped to the regions of the augmented face.
+        ModelRenderable.builder()
+                .setSource(this, R.raw.fox_face)
+                .build()
+                .thenAccept(
+                        modelRenderable -> {
+                            faceRegionsRenderable = modelRenderable;
+                            modelRenderable.setShadowCaster(false);
+                            modelRenderable.setShadowReceiver(false);
+                        });
+
+        // Load the face mesh texture.
+        Texture.builder()
+                .setSource(this, R.drawable.fox_face_mesh_texture)
+                .build()
+                .thenAccept(texture -> faceMeshTexture = texture);
+
+        ArSceneView sceneView = arFragment.getArSceneView();
+
+        // This is important to make sure that the camera stream renders first so that
+        // the face mesh occlusion works correctly.
+        sceneView.setCameraStreamRenderPriority(Renderable.RENDER_PRIORITY_FIRST);
+
+        Scene scene = sceneView.getScene();
+
+        scene.addOnUpdateListener(
+                (FrameTime frameTime) -> {
+                    if (faceRegionsRenderable == null || faceMeshTexture == null) {
+                        return;
+                    }
+
+                    Collection<AugmentedFace> faceList =
+                            sceneView.getSession().getAllTrackables(AugmentedFace.class);
+
+                    // Make new AugmentedFaceNodes for any new faces.
+                    for (AugmentedFace face : faceList) {
+                        if (!faceNodeMap.containsKey(face)) {
+                            AugmentedFaceNode faceNode = new AugmentedFaceNode(face);
+                            faceNode.setParent(scene);
+                            faceNode.setFaceRegionsRenderable(faceRegionsRenderable);
+                            faceNode.setFaceMeshTexture(faceMeshTexture);
+                            faceNodeMap.put(face, faceNode);
+                        }
+                    }
+
+                    // Remove any AugmentedFaceNodes associated with an AugmentedFace that stopped tracking.
+                    Iterator<Map.Entry<AugmentedFace, AugmentedFaceNode>> iter =
+                            faceNodeMap.entrySet().iterator();
+                    while (iter.hasNext()) {
+                        Map.Entry<AugmentedFace, AugmentedFaceNode> entry = iter.next();
+                        AugmentedFace face = entry.getKey();
+                        if (face.getTrackingState() == TrackingState.STOPPED) {
+                            AugmentedFaceNode faceNode = entry.getValue();
+                            faceNode.setParent(null);
+                            iter.remove();
+                        }
+                    }
+                });
     }
 
     private void CheckCamera(){
@@ -301,6 +399,27 @@ public class Home extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(
                 "content://media/internal/images/media"));
         startActivity(intent);
+    }
+
+    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+        if (Build.VERSION.SDK_INT < VERSION_CODES.N) {
+            Log.e(TAG, "Sceneform requires Android N or later");
+            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
+            activity.finish();
+            return false;
+        }
+        String openGlVersionString =
+                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+                        .getDeviceConfigurationInfo()
+                        .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                    .show();
+            activity.finish();
+            return false;
+        }
+        return true;
     }
 
 }
